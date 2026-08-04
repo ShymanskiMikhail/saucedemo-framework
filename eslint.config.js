003344aa37051cc259ee245505971e749c2c4fc9
Mikhail Shymanski <misasimanski@gmail.com>
2026-08-05 01:16:33 +0300

    configuration setup

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: { project: null },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'off',
+    },
+  },
+];
